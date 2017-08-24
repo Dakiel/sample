bfa3f32a7f3960431c325a345c083ea84391b635
Dakiel <chaossedum@gmail.com>
2017-08-24 08:30:07 +0000

    Finish login and logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if(Auth::attempt($credentials, $request->has('remember'))) {
+            session()->flash('success', '欢迎回来! ');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', 'Sorry');
+            return redirect()->back();
+        }
+
+        return;
+    }
+}
